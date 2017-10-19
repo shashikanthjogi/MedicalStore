f53c9fc79c4464bfb1f2abe49eb319d13ee82763
shashikanth <shashikanth.jogi@gmail.com>
2017-10-19 22:09:54 +0530

    First Commit

---
+++ MedicalStore/UserLogin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedicalStore
+{
+    public partial class UserLogin : Form
+    {
+        public UserLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+    }
+}
